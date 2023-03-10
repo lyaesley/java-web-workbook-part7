5fd338bb9cabcb369c9106b5fb34da4bbd62e62e
lyae <lyaesley@gmail.com>
2023-03-10 16:19:58 +0900

    [java-web-workbook] 7장 파일 업로드 처리

---
+++ src/main/java/com/lyae/workbook/b01/dto/upload/UploadFileDTO.java
@@ -0,0 +1,11 @@
+package com.lyae.workbook.b01.dto.upload;
+
+import lombok.Data;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@Data
+public class UploadFileDTO {
+    private List<MultipartFile> files;
+}
